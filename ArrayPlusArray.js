da74b9770afd1b83eba436672dea78f0f715e02b
mihaGP800 <mihagoroh@gmail.com>
2022-03-05 15:21:18 +0300

    test2

--- ArrayPlusArray.js
+++ ArrayPlusArray.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function arrayPlusArray(arr1, arr2) {
     return arr1.reduce((acc,el)=> acc + el) + arr2.reduce((acc,el)=> acc + el); //something went wrong
 
@@ -11,18 +10,4 @@ function arrayPlusArray(arr1, arr2) {
 
     [...arr1, ...arr2].reduce((a, b) => a + b, 0);
 }
-=======
-function arrayPlusArray(arr1, arr2) {
-    return arr1.reduce((acc,el)=> acc + el) + arr2.reduce((acc,el)=> acc + el); //something went wrong
-
-    return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
-
-    return [].concat(...arrays).reduce((a,b) => a+b,0)
-
-    let arr = [...arr1, ...arr2];
-    return arr.reduce((a, b) => a + b);
-
-    [...arr1, ...arr2].reduce((a, b) => a + b, 0);
-}
->>>>>>> 6bff727e70845de3b244408d39ce101a90253026
 arrayPlusArray([1, 2, 3], [4, 5, 6])
--- reversedSequence.js
+++ reversedSequence.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const reverseSeq = n => {
     let arr = []
     for (let i = n; i > 0; i-=1) {
@@ -8,15 +7,4 @@ const reverseSeq = n => {
 
     return console.log(Array(n).fill(0).map((e, i) => n - i ));
 };
-=======
-const reverseSeq = n => {
-    let arr = []
-    for (let i = n; i > 0; i-=1) {
-       arr.push(i)
-    }
-    return arr
-
-    // return console.log(Array(n).fill(0).map((e, i) => n - i ));
-};
->>>>>>> 6bff727e70845de3b244408d39ce101a90253026
 reverseSeq(5)
