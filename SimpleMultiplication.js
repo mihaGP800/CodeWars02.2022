94d4dd8789a49524a13896c9a1d9eb9b8b6ba8ee
mihaGP800 <mihagoroh@gmail.com>
2022-03-08 08:56:34 +0300

    SimpleMultiplication

---
+++ SimpleMultiplication.js
@@ -0,0 +1,10 @@
+function simpleMultiplication(number) {
+    console.log(number%2===0 ? number*8  : number*9)
+    // your code........
+
+    return n * (n % 2 ? 9 : 8);
+}
+simpleMultiplication(2)
+simpleMultiplication(1)
+simpleMultiplication(3)
+simpleMultiplication(4)
