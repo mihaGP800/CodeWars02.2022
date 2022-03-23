1c311a0d4496a85b76089083073fdb505329c269
mihaGP800 <mihagoroh@gmail.com>
2022-03-23 19:51:02 +0300

    sortStar

---
+++ SortStar.js
@@ -0,0 +1,6 @@
+function twoSort(s) {
+    console.log(s.sort()[0].split('').join('***'))
+}
+
+twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"])
+twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"])
