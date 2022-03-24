24e89fa8500c252cecc67041b0af2d11afd3f4c5
mihaGP800 <mihagoroh@gmail.com>
2022-03-24 08:52:11 +0300

    squashBugs

---
+++ KeepUpHoop.js
@@ -0,0 +1,6 @@
+function hoopCount (n) {
+    console.log(n>=10 ?  "Great, now move on to tricks" : "Keep at it until you get it")
+}
+
+hoopCount(3)
+hoopCount(10)
---
+++ SquashBugs.js
@@ -0,0 +1,15 @@
+function findLongest(str) {
+
+    let spl = str.split(" ");
+    let longest = 0
+
+    for (let i = 0; i < spl.length; i++) {
+        if (spl[i].length > longest) {
+            longest = spl[i].length
+        }
+    }
+    return console.log(longest)
+}
+
+findLongest("The quick white fox jumped around the massive dog")
+findLongest("Take me to tinseltown with you")
