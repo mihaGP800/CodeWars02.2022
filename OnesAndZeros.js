2e686b5155894fe38430a08db74e71b1976585ce
mihaGP800 <mihagoroh@gmail.com>
2022-09-16 10:01:33 +0300

    binary_7kyu

---
+++ OnesAndZeros.js
@@ -0,0 +1,11 @@
+const binaryArrayToNumber = arr => {
+    return parseInt(arr.join(''), 2)
+
+
+    // return arr.reduce((total, cur) => (total = total * 2 + cur), 0);
+};
+
+console.log(binaryArrayToNumber([0,0,0,1])) // 1
+console.log(binaryArrayToNumber([0,0,1,0])) // 2
+console.log(binaryArrayToNumber([1,1,1,1])) // 15
+console.log(binaryArrayToNumber([0,1,1,0])) // 6
