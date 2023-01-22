1aca6725066d418c86bf5acf6735275dc806a170
MishaGoroshko <mihagoroh@gmail.com>
2023-01-22 10:18:14 +0300

    recursion_6kyu

---
+++ ArrayDeepCount.js
@@ -0,0 +1,24 @@
+function deepCount(a) {
+    let length = a.length
+
+    function flatten(array) {
+        let res = []
+
+        for (let i = 0; i < array.length; i++) {
+            if (Array.isArray(array[i])) {
+                length += array[i].length
+                res = [...res, ...flatten(array[i])]
+            } else {
+                res.push(array[i])
+            }
+        }
+        return res
+    }
+    flatten(a)
+
+    return length
+}
+
+console.log(deepCount([]))
+console.log(deepCount([1, 2, 3]))
+console.log(deepCount([1, 2, [3, 4, [5]]]))
